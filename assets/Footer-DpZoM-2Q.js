60890080ad57962036af25250f0e9b0cb417a272
Belinda Wewalage <belinda@Belindas-MacBook-Pro.local>
2025-06-20 12:04:55 +0100

    Updates

--- assets/Footer-DpZoM-2Q.js
+++ assets/Footer-DpZoM-2Q.js
@@ -1,4 +1,4 @@
-import{c as d,j as e,r as m,v as c,L as o,B as i,X as x}from"./index-knhxRz3z.js";/**
+import{c as d,j as e,r as m,v as c,L as o,B as i,X as x}from"./index-D2OVY4va.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
