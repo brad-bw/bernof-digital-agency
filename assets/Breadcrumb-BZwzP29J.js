216156ac0528d4c1f27ffde783e17b3fa4f04f33
Belinda Wewalage <belinda@Belindas-MacBook-Pro.local>
2025-06-28 01:24:06 +0100

    Updates

--- assets/Breadcrumb-BZwzP29J.js
+++
@@ -1,6 +0,0 @@
-import{c as t,j as a,L as n}from"./index-Dt4s45kz.js";import{C as l}from"./chevron-right-BmpQyWSc.js";/**
- * @license lucide-react v0.462.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const c=t("House",[["path",{d:"M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8",key:"5wwlr5"}],["path",{d:"M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z",key:"1d0kgt"}]]),x=({items:e})=>!e||e.length===0?null:a.jsx("nav",{"aria-label":"Breadcrumb",className:"py-4",children:a.jsx("ol",{className:"flex items-center space-x-2 text-sm text-gray-600",children:e.map((s,r)=>a.jsxs("li",{className:"flex items-center",children:[r===0&&a.jsx(c,{className:"w-4 h-4 mr-1"}),r<e.length-1?a.jsxs(a.Fragment,{children:[a.jsx(n,{to:s.url,className:"hover:text-brand-teal transition-colors font-inter",children:s.name}),a.jsx(l,{className:"w-4 h-4 mx-2 text-gray-400"})]}):a.jsx("span",{className:"text-gray-900 font-medium font-inter",children:s.name})]},r))})});export{x as B};
