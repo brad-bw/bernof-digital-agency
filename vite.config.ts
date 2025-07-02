6c094761b5dbab6eae3f233e58f23432a82d3fb5
Belinda Wewalage <belinda@Belindas-MacBook-Pro.local>
2025-07-02 22:53:41 +0100

    Add vite-plugin-sitemap for automatic sitemap generation

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import Sitemap from 'vite-plugin-sitemap'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
