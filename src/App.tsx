ea19ff214bc2dd5df0dbf48191ba23623bd057b6
Belinda Wewalage <belinda@Belindas-MacBook-Pro.local>
2025-07-03 23:24:05 +0100

    Loop issue Fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,72 @@
+import { Suspense, lazy } from "react"
+import { Toaster as RadixToaster } from "@/components/ui/toaster"
+import { Toaster as SonnerToaster } from "@/components/ui/sonner"
+import { TooltipProvider } from "@/components/ui/tooltip"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { HelmetProvider } from "react-helmet-async"
+import { ThemeProvider } from "@/components/ThemeProvider"
+import { AnalyticsProvider } from "@/components/AnalyticsProvider"
+import ErrorBoundary from "@/components/ErrorBoundary"
+import Loading from "@/components/Loading"
 
-import { Suspense, lazy } from "react";
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { HelmetProvider } from 'react-helmet-async';
-import { ThemeProvider } from "@/components/ThemeProvider";
-import { AnalyticsProvider } from "@/components/AnalyticsProvider";
-import ErrorBoundary from "@/components/ErrorBoundary";
-import Loading from "@/components/Loading";
+const Index = lazy(() => import("./pages/Index"))
+const Blog = lazy(() => import("./pages/Blog"))
+const BlogPost = lazy(() => import("./pages/BlogPost"))
+const GlobalServices = lazy(() => import("./pages/GlobalServices"))
+const StartupDevelopment = lazy(() => import("./pages/StartupDevelopment"))
+const VibeCoding = lazy(() => import("./pages/VibeCoding"))
+const PrivacyPolicy = lazy(() => import("./pages/PrivacyPolicy"))
+const TermsOfService = lazy(() => import("./pages/TermsOfService"))
+const CookiePolicy = lazy(() => import("./pages/CookiePolicy"))
+const NotFound = lazy(() => import("./pages/NotFound"))
 
-const Index = lazy(() => import("./pages/Index"));
-const Blog = lazy(() => import("./pages/Blog"));
-const BlogPost = lazy(() => import("./pages/BlogPost"));
-const GlobalServices = lazy(() => import("./pages/GlobalServices"));
-const StartupDevelopment = lazy(() => import("./pages/StartupDevelopment"));
-const VibeCoding = lazy(() => import("./pages/VibeCoding"));
-const PrivacyPolicy = lazy(() => import("./pages/PrivacyPolicy"));
-const TermsOfService = lazy(() => import("./pages/TermsOfService"));
-const CookiePolicy = lazy(() => import("./pages/CookiePolicy"));
-const NotFound = lazy(() => import("./pages/NotFound"));
+const SwitzerlandHub = lazy(() => import("./pages/countries/SwitzerlandHub"))
+const UKHub = lazy(() => import("./pages/countries/UKHub"))
+const USHub = lazy(() => import("./pages/countries/USHub"))
+const ItalyHub = lazy(() => import("./pages/countries/ItalyHub"))
 
-const SwitzerlandHub = lazy(() => import("./pages/countries/SwitzerlandHub"));
-const UKHub = lazy(() => import("./pages/countries/UKHub"));
-const USHub = lazy(() => import("./pages/countries/USHub"));
-const ItalyHub = lazy(() => import("./pages/countries/ItalyHub"));
-const SwitzerlandWebDevelopment = lazy(() => import("./pages/countries/SwitzerlandWebDevelopment"));
-const SwitzerlandSoftwareDevelopment = lazy(() => import("./pages/countries/SwitzerlandSoftwareDevelopment"));
-const SwitzerlandStartupDevelopment = lazy(() => import("./pages/countries/SwitzerlandStartupDevelopment"));
-const UKWebDevelopment = lazy(() => import("./pages/countries/UKWebDevelopment"));
-const UKSoftwareDevelopment = lazy(() => import("./pages/countries/UKSoftwareDevelopment"));
-const UKStartupDevelopment = lazy(() => import("./pages/countries/UKStartupDevelopment"));
-const USAWebDevelopment = lazy(() => import("./pages/countries/USAWebDevelopment"));
-const USASoftwareDevelopment = lazy(() => import("./pages/countries/USASoftwareDevelopment"));
-const USAStartupDevelopment = lazy(() => import("./pages/countries/USAStartupDevelopment"));
-const ItalyWebDevelopment = lazy(() => import("./pages/countries/ItalyWebDevelopment"));
-const ItalySoftwareDevelopment = lazy(() => import("./pages/countries/ItalySoftwareDevelopment"));
-const ItalyStartupDevelopment = lazy(() => import("./pages/countries/ItalyStartupDevelopment"));
+const SwitzerlandWebDevelopment = lazy(
+  () => import("./pages/countries/SwitzerlandWebDevelopment")
+)
+const SwitzerlandSoftwareDevelopment = lazy(
+  () => import("./pages/countries/SwitzerlandSoftwareDevelopment")
+)
+const SwitzerlandStartupDevelopment = lazy(
+  () => import("./pages/countries/SwitzerlandStartupDevelopment")
+)
 
-const queryClient = new QueryClient();
+const UKWebDevelopment = lazy(() => import("./pages/countries/UKWebDevelopment"))
+const UKSoftwareDevelopment = lazy(
+  () => import("./pages/countries/UKSoftwareDevelopment")
+)
+const UKStartupDevelopment = lazy(
+  () => import("./pages/countries/UKStartupDevelopment")
+)
 
-const App = () => {
+const USAWebDevelopment = lazy(
+  () => import("./pages/countries/USAWebDevelopment")
+)
+const USASoftwareDevelopment = lazy(
+  () => import("./pages/countries/USASoftwareDevelopment")
+)
+const USAStartupDevelopment = lazy(
+  () => import("./pages/countries/USAStartupDevelopment")
+)
+
+const ItalyWebDevelopment = lazy(
+  () => import("./pages/countries/ItalyWebDevelopment")
+)
+const ItalySoftwareDevelopment = lazy(
+  () => import("./pages/countries/ItalySoftwareDevelopment")
+)
+const ItalyStartupDevelopment = lazy(
+  () => import("./pages/countries/ItalyStartupDevelopment")
+)
+
+const queryClient = new QueryClient()
+
+function App() {
   return (
     <ErrorBoundary>
       <HelmetProvider>
@@ -50,40 +75,68 @@ const App = () => {
             <TooltipProvider>
               <BrowserRouter>
                 <AnalyticsProvider>
-                  <Toaster />
-                  <Sonner />
+                  <RadixToaster />
+                  <SonnerToaster />
                   <Suspense fallback={<Loading />}>
                     <Routes>
                       <Route path="/" element={<Index />} />
                       <Route path="/blog" element={<Blog />} />
                       <Route path="/blog/:slug" element={<BlogPost />} />
+
                       <Route path="/global-services" element={<GlobalServices />} />
                       <Route path="/global-services/ch" element={<SwitzerlandHub />} />
                       <Route path="/global-services/uk" element={<UKHub />} />
                       <Route path="/global-services/us" element={<USHub />} />
                       <Route path="/global-services/it" element={<ItalyHub />} />
+
                       <Route path="/startup-development" element={<StartupDevelopment />} />
                       <Route path="/vibe-coding" element={<VibeCoding />} />
+
                       <Route path="/privacy-policy" element={<PrivacyPolicy />} />
                       <Route path="/terms-of-service" element={<TermsOfService />} />
                       <Route path="/cookie-policy" element={<CookiePolicy />} />
-                      
+
                       <Route path="/ch/web-development" element={<SwitzerlandWebDevelopment />} />
-                      <Route path="/ch/software-development" element={<SwitzerlandSoftwareDevelopment />} />
-                      <Route path="/ch/startup-development" element={<SwitzerlandStartupDevelopment />} />
-                      
+                      <Route
+                        path="/ch/software-development"
+                        element={<SwitzerlandSoftwareDevelopment />}
+                      />
+                      <Route
+                        path="/ch/startup-development"
+                        element={<SwitzerlandStartupDevelopment />}
+                      />
+
                       <Route path="/uk/web-development" element={<UKWebDevelopment />} />
-                      <Route path="/uk/software-development" element={<UKSoftwareDevelopment />} />
-                      <Route path="/uk/startup-development" element={<UKStartupDevelopment />} />
-                      
+                      <Route
+                        path="/uk/software-development"
+                        element={<UKSoftwareDevelopment />}
+                      />
+                      <Route
+                        path="/uk/startup-development"
+                        element={<UKStartupDevelopment />}
+                      />
+
                       <Route path="/us/web-development" element={<USAWebDevelopment />} />
-                      <Route path="/us/software-development" element={<USASoftwareDevelopment />} />
-                      <Route path="/us/startup-development" element={<USAStartupDevelopment />} />
-                      
+                      <Route
+                        path="/us/software-development"
+                        element={<USASoftwareDevelopment />}
+                      />
+                      <Route
+                        path="/us/startup-development"
+                        element={<USAStartupDevelopment />}
+                      />
+
                       <Route path="/it/web-development" element={<ItalyWebDevelopment />} />
-                      <Route path="/it/software-development" element={<ItalySoftwareDevelopment />} />
-                      <Route path="/it/startup-development" element={<ItalyStartupDevelopment />} />
-                      
+                      <Route
+                        path="/it/software-development"
+                        element={<ItalySoftwareDevelopment />}
+                      />
+                      <Route
+                        path="/it/startup-development"
+                        element={<ItalyStartupDevelopment />}
+                      />
+
+                      {/* Catch-all route */}
                       <Route path="*" element={<NotFound />} />
                     </Routes>
                   </Suspense>
@@ -94,7 +147,7 @@ const App = () => {
         </ThemeProvider>
       </HelmetProvider>
     </ErrorBoundary>
-  );
-};
+  )
+}
 
-export default App;
+export default App
