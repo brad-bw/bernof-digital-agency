249faf8b8f65be45938089e4c6ea331a4c3fe37c
Belinda MW <bmw.aixplore@gmail.com>
2025-07-06 19:58:34 +0100

    Debug: Use minimal Sanity query to isolate 400 error

--- src/utils/sanityClient.ts
+++ src/utils/sanityClient.ts
@@ -11,45 +11,6 @@ export default sanityClient;
 
 // Example GROQ query to fetch all blog posts with all fields
 export async function fetchBlogPosts() {
-  return sanityClient.fetch(`*[_type == "blogPost"] | order(publishedAt desc) {
-    _id,
-    metaTitle,
-    metaDescription,
-    slug,
-    featuredImage {
-      asset->{url},
-      alt
-    },
-    excerpt,
-    body,
-    categories,
-    tags,
-    author {
-      name,
-      bio,
-      image {
-        asset->{url}
-      },
-      twitter,
-      linkedin,
-      website
-    },
-    publishedAt,
-    focusKeyword,
-    schemaType,
-    readingTime,
-    relatedPosts[]->{
-      _id,
-      metaTitle,
-      slug
-    },
-    openGraphImage {
-      asset->{url}
-    },
-    twitterCardType,
-    socialDescription,
-    canonicalUrl,
-    noIndex,
-    customScripts
-  }`)
+  // Minimal query for debugging
+  return sanityClient.fetch(`*[_type == "blogPost"][0...1]{_id, metaTitle}`)
 } 
